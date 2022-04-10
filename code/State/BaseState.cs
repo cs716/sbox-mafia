e9b0b915a072683fa1e9af6a901720fdf55e4fe8
Cameron Smith <805camerons@gmail.com>
2022-04-10 00:30:34 -0400

    Add BaseState file and folder structure

---
+++ code/State/BaseState.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+
+using TerryTrials.Player;
+
+namespace TerryTrials.State
+{
+	public abstract partial class BaseState : BaseNetworkable
+	{
+		[Net] public virtual RealTimeUntil StateTimer { get; set; } = 0f;
+
+		public void Start()
+		{
+			OnStart();
+		}
+
+		public void Finish()
+		{
+			OnFinish();
+		}
+
+		protected virtual void OnStart() { }
+		protected virtual void OnFinish() { }
+
+		public virtual void OnPlayerLeave() { }
+		public virtual void OnPlayerJoin(MafiaPlayer player) { }
+		public virtual void OnTick() { }
+		public virtual void OnSecond() { }
+
+	}
+}
