61512cf0e3c0922bdab5b456a945f16be93e35d2
Cameron Smith <805camerons@gmail.com>
2022-04-10 00:30:16 -0400

    Redo how the default gamemode handles spawns

--- code/Game.cs
+++ code/Game.cs
@@ -2,14 +2,16 @@
 using Sandbox;
 using Sandbox.UI.Construct;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using TerryTrials.Player;
 
 //
 // You don't need to put things in a namespace, but it doesn't hurt.
 //
-namespace Sandbox
+namespace TerryTrials
 {
 	/// <summary>
 	/// This is your game class. This is an entity that is created serverside when
@@ -22,6 +24,7 @@ namespace Sandbox
 	{
 		public TerryTrials()
 		{
+
 		}
 
 		/// <summary>
@@ -32,22 +35,29 @@ namespace Sandbox
 			base.ClientJoined( client );
 
 			// Create a pawn for this client to play with
-			var pawn = new Pawn();
-			client.Pawn = pawn;
+			var player = new MafiaPlayer( client );
+			client.Pawn = player;
 
 			// Get all of the spawnpoints
 			var spawnpoints = Entity.All.OfType<SpawnPoint>();
 
-			// chose a random one
-			var randomSpawnPoint = spawnpoints.OrderBy( x => Guid.NewGuid() ).FirstOrDefault();
-
-			// if it exists, place the pawn there
-			if ( randomSpawnPoint != null )
+			foreach( var spawnPoint in spawnpoints )
 			{
-				var tx = randomSpawnPoint.Transform;
-				tx.Position = tx.Position + Vector3.Up * 50.0f; // raise it up
-				pawn.Transform = tx;
+				bool available = !All.OfType<MafiaPlayer>().Where( player => player.SpawnPointId == spawnPoint.NetworkIdent ).Any();
+				if (available)
+				{
+					player.SpawnPointId = spawnPoint.NetworkIdent;
+					player.Position = spawnPoint.Position;
+					player.Rotation = spawnPoint.Rotation;
+
+					player.Respawn();
+				}
 			}
+		}
+
+		public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
+		{
+			base.ClientDisconnect( cl, reason );
 		}
 	}
 
