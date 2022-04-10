a98716c3f0880e030868a711c9d54c427185d65b
Cameron Smith <805camerons@gmail.com>
2022-04-10 00:30:43 -0400

    Add MafiaPlayer as player entity

---
+++ code/Player/MafiaPlayer.cs
@@ -0,0 +1,44 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerryTrials.Player
+{
+	public partial class MafiaPlayer : Sandbox.Player
+	{
+		[Net] public int SpawnPointId { get; set; }
+
+		public Clothing.Container Clothing = new();
+
+		public MafiaPlayer()
+		{
+
+		}
+
+		public MafiaPlayer(Client client) : this()
+		{
+			Clothing.LoadFromClient( client );
+		}
+
+		public override void Respawn()
+		{
+			CameraMode = new ThirdPersonCamera();
+			Animator = new StandardPlayerAnimator();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+
+			SetModel( "models/citizen/citizen.vmdl" );
+
+			foreach ( var child in Children )
+				child.EnableDrawing = true;
+
+			Clothing.DressEntity( this );
+
+			base.Respawn();
+		}
+	}
+}
