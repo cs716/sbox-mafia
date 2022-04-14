f18578a946a247745537f124e396dc6eae3ff92b
Cameron Smith <805camerons@gmail.com>
2022-04-13 22:05:01 -0400

    Add IdleState and LobbyState

---
+++ code/State/LobbyState.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerryTrials.Player;
+using TerryTrials.State;
+
+namespace TerryTrials.State
+{
+	public partial class LobbyState : BaseState
+	{
+		public override void OnPlayerJoin(MafiaPlayer player)
+		{
+			var spawnpoints = Entity.All.OfType<SpawnPoint>();
+
+			foreach ( var spawnPoint in spawnpoints )
+			{
+				bool available = !Entity.All.OfType<MafiaPlayer>().Where( player => player.SpawnPointId == spawnPoint.NetworkIdent ).Any();
+				if ( available )
+				{
+					player.SpawnPointId = spawnPoint.NetworkIdent;
+
+					player.Position = spawnPoint.Position;
+					player.Rotation = spawnPoint.Rotation;
+
+					player.Respawn();
+				}
+			}
+		}
+	}
+}
