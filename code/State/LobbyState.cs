deda46fc2b90c4ba873dc3704593f713f67adf18
Cameron Smith <805camerons@gmail.com>
2022-04-14 23:59:05 -0400

    Modify player spawn logic to space players out during Lobby phase

--- code/State/LobbyState.cs
+++ code/State/LobbyState.cs
@@ -11,21 +11,34 @@ namespace TerryTrials.State
 {
 	public partial class LobbyState : BaseState
 	{
+
+		public override void OnStart()
+		{
+			base.OnStart();
+			// Find any existing players that joined during Idle and initialize them
+
+			var players = Entity.All.OfType<MafiaPlayer>();
+			if (players.Any())
+			{
+				foreach (var player in players.ToList())
+				{
+					OnPlayerJoin( player );
+				}
+			}
+		}
 		public override void OnPlayerJoin(MafiaPlayer player)
 		{
 			var spawnpoints = Entity.All.OfType<SpawnPoint>();
 
-			foreach ( var spawnPoint in spawnpoints )
+			foreach ( var spawnPoint in spawnpoints.ToList() )
 			{
-				bool available = !Entity.All.OfType<MafiaPlayer>().Where( player => player.SpawnPointId == spawnPoint.NetworkIdent ).Any();
-				if ( available )
+				Vector3 potentialSpawn = spawnPoint.Position;
+				bool occupied = Entity.FindInSphere( potentialSpawn, 5 ).OfType<MafiaPlayer>().Where( p => p.IsAlive ).Any();
+				if ( !occupied )
 				{
-					player.SpawnPointId = spawnPoint.NetworkIdent;
-
-					player.Position = spawnPoint.Position;
-					player.Rotation = spawnPoint.Rotation;
-
 					player.Respawn();
+					player.Position = potentialSpawn;
+					player.Rotation = spawnPoint.Rotation;
 				}
 			}
 		}
