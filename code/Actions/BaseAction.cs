02b5dc2267037ec5e9e62bfdd3e21263570920de
Cameron Smith <805camerons@gmail.com>
2022-04-17 02:35:55 -0400

    Add BaseAction

---
+++ code/Actions/BaseAction.cs
@@ -0,0 +1,17 @@
+﻿namespace TerryTrials.Actions;
+
+public abstract partial class BaseAction
+{
+	public virtual string Name { get; set; } = "default";
+	public virtual string Message { get; set; }
+
+	public void Execute()
+	{
+		OnExecute();
+		OnExecuted();
+	}
+
+	public virtual void OnExecute() { }
+	public virtual void OnExecuted() { }
+}
+
