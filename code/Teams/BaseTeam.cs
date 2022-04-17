e661757ef2626bbaa47088abdc66cfaba0a26cfb
Cameron Smith <805camerons@gmail.com>
2022-04-17 02:37:07 -0400

    Add Teams

---
+++ code/Teams/BaseTeam.cs
@@ -0,0 +1,25 @@
+﻿using TerryTrials.Actions;
+using TerryTrials.Player;
+
+namespace TerryTrials.Teams;
+public enum Alliance
+{
+	Town,
+	Mafia,
+	ThirdParty = -1
+}
+
+public abstract partial class BaseTeam
+{
+	public virtual Alliance Alliance => Alliance.ThirdParty; // Town, Mafia or ThirdParty. Controls point assignments on a win condition
+	public virtual string SingularName => "Unknown"; // Friendly name for the role in singular form
+	public virtual string PluralName => "Unknown"; // Friendly name for the role in plural form
+	public virtual BaseAction NightAction => null; // Night action for this role (if any)
+	public virtual BaseAction DayAction => null; // Day action for this role (if any)
+	public virtual int MinimumPlayers => 0; // Minimum players that must be playing for this role to become active in the rotation
+	public virtual double GetAssignRatio()
+	{
+		return 0.0;
+	}
+	public virtual void OnPlayerChat( MafiaPlayer player, string Message ) { }
+}
---
+++ code/Teams/MafiaTeam.cs
@@ -0,0 +1,23 @@
+﻿using TerryTrials.Player;
+
+namespace TerryTrials.Teams;
+
+public partial class MafiaTeam : BaseTeam
+{
+	[ServerVar("tt_mafia_player_ratio", Help = "Ratio of Town to Mafia players", Min = 0.4f, Max = 0.1f)]
+	public static double MafiaPlayerRatio { get; set; } = 0.25;
+	public override Alliance Alliance => Alliance.Mafia;
+	public override string SingularName => "Mafia";
+	public override string PluralName => "the Mafia";
+	public override int MinimumPlayers => 0;
+	public override double GetAssignRatio()
+	{
+		return MafiaPlayerRatio;
+	}
+
+	public override void OnPlayerChat( MafiaPlayer player, string Message )
+	{
+		base.OnPlayerChat( player, Message );
+	}
+}
+
