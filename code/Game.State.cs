9ea2ab4e6cbc51cef467f995f5d38f23de499ed0
Cameron Smith <805camerons@gmail.com>
2022-04-17 02:37:16 -0400

    Refactor

--- code/Game.State.cs
+++ code/Game.State.cs
@@ -1,23 +1,16 @@
 ﻿using Sandbox;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TerryTrials.State;
 
-namespace TerryTrials
+namespace TerryTrials;
+partial class Game
 {
-	partial class Game
-	{
-		[Net, Change( nameof( OnStateChange ) )] public BaseState GameState { get; private set; } 
+	[Net, Change( nameof( OnStateChange ) )] public BaseState GameState { get; private set; }
 
-		public void ChangeState(BaseState newState)
-		{
-			Assert.NotNull( newState );
-			GameState?.Finish();
-			GameState = newState;
-			GameState?.Start();
-		}
+	public void ChangeState( BaseState newState )
+	{
+		Assert.NotNull( newState );
+		GameState?.Finish();
+		GameState = newState;
+		GameState?.Start();
 	}
 }
--- code/Game.cs
+++ code/Game.cs
@@ -1,71 +1,58 @@
-﻿
-using Sandbox;
-using Sandbox.UI.Construct;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Sandbox;
 using TerryTrials.Player;
 using TerryTrials.State;
 
-//
-// You don't need to put things in a namespace, but it doesn't hurt.
-//
-namespace TerryTrials
+namespace TerryTrials;
+/// <summary>
+/// This is your game class. This is an entity that is created serverside when
+/// the game starts, and is replicated to the client. 
+/// 
+/// You can use this to create things like HUDs and declare which player class
+/// to use for spawned players.
+/// </summary>
+partial class Game : Sandbox.Game
 {
-	/// <summary>
-	/// This is your game class. This is an entity that is created serverside when
-	/// the game starts, and is replicated to the client. 
-	/// 
-	/// You can use this to create things like HUDs and declare which player class
-	/// to use for spawned players.
-	/// </summary>
-	partial class Game : Sandbox.Game
+	public static Game Instance
 	{
-		public static Game Instance
-		{
-			get => Current as Game;
-		}
-
-		public Game()
-		{
-			if (IsServer)
-			{
-				Global.TickRate = 30;
-			}
-			ChangeState( new IdleState() );
-		}
-
-		/// <summary>
-		/// A client has joined the server. Make them a pawn to play with
-		/// </summary>
-		public override void ClientJoined( Client client )
-		{
-			base.ClientJoined( client );
-
-			var player = new MafiaPlayer( client );
-			client.Pawn = player;
-
-			GameState?.OnPlayerJoin( player );
-		}
-
-		public override void PostLevelLoaded()
-		{
-			ChangeState( new LobbyState() );
-			base.PostLevelLoaded();
-		}
-
-		public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
-		{
-			base.ClientDisconnect( cl, reason );
-		}
-
-		private void OnStateChange(BaseState lastState, BaseState newState)
-		{
-			lastState?.Finish();
-			newState?.Start();
-		}
+		get => Current as Game;
 	}
 
+	public Game()
+	{
+		if ( IsServer )
+		{
+			Global.TickRate = 30;
+		}
+		ChangeState( new IdleState() );
+	}
+
+	/// <summary>
+	/// A client has joined the server. Make them a pawn to play with
+	/// </summary>
+	public override void ClientJoined( Client client )
+	{
+		base.ClientJoined( client );
+
+		var player = new MafiaPlayer( client );
+		client.Pawn = player;
+
+		GameState?.OnPlayerJoin( player );
+	}
+
+	public override void PostLevelLoaded()
+	{
+		ChangeState( new LobbyState() );
+		base.PostLevelLoaded();
+	}
+
+	public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
+	{
+		base.ClientDisconnect( cl, reason );
+	}
+
+	private void OnStateChange( BaseState lastState, BaseState newState )
+	{
+		lastState?.Finish();
+		newState?.Start();
+	}
 }
