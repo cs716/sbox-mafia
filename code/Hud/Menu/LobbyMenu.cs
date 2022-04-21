6b123668d294c5f647d8219de14e97ec9dfa2660
Cameron Smith <805camerons@gmail.com>
2022-04-21 16:25:31 -0400

    Add ReadyStateLocked

--- code/Hud/Menu/LobbyMenu.cs
+++ code/Hud/Menu/LobbyMenu.cs
@@ -14,6 +14,15 @@ public partial class LobbyMenu : Panel
 {
 	Label ReadyStatus { get; set; }
 	Panel ReadyPanel { get; set; }
+	public static bool ReadyStateLocked { 
+		get
+		{
+			if ( Game.Instance.GameState is LobbyState state )
+				return state.ReadyStateLocked;
+			else
+				return false;
+		}
+	}
 	public static bool IsReady
 	{
 		get => ((MafiaPlayer)Local.Pawn).IsReady;
@@ -32,8 +41,7 @@ public partial class LobbyMenu : Panel
 		{
 			foreach ( var player in Entity.All.OfType<MafiaPlayer>())
 			{
-				NameTagPanel panel;
-				bool getPanel = Game.Instance.NameTagHandler.NameTags.TryGetValue( player, out panel );
+				bool getPanel = Game.Instance.NameTagHandler.NameTags.TryGetValue( player, out NameTagPanel panel );
 				if ( getPanel )
 				{
 					panel.StatusLabel.Text = player.IsReady ? "Ready" : "Not Ready";
@@ -44,6 +52,7 @@ public partial class LobbyMenu : Panel
 			}
 			ReadyStatus.Text = IsReady ? "Ready" : "Not Ready";
 			ReadyStatus.SetClass( "ready", IsReady );
+			Log.Info( "Time Left " + ((LobbyState)Game.Instance.GameState).LobbyCountdown );
 		}
 		base.Tick();
 	}
@@ -52,7 +61,7 @@ public partial class LobbyMenu : Panel
 	public void ProcessBuildInput( InputBuilder inputBuilder )
 	{
 		Assert.NotNull( inputBuilder );
-		if (!HasClass("hide"))
+		if (!HasClass("hide") && ReadyStateLocked == false)
 		{
 			if (inputBuilder.Pressed(InputButton.Jump))
 			{
