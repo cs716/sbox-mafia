e7684a13abe0703d768c57593a9171d4f5594054
Cameron Smith <805camerons@gmail.com>
2022-04-20 20:24:26 -0400

    Create Action types

---
+++ code/Actions/SinglePlayerVoteSelector/BaseSinglePlayerVoteSelector.cs
@@ -0,0 +1,20 @@
+﻿using Sandbox;
+using System.Collections;
+using System.Collections.Generic;
+using TerryTrials.Player;
+
+namespace TerryTrials.Actions.SinglePlayerVoteSelector;
+public partial class BaseSinglePlayerVoteSelector : BaseAction
+{
+	private protected Dictionary<MafiaPlayer, MafiaPlayer> Votes; // <MafiaPlayer VOTER, MafiaPlayer VOTEE>
+
+	public virtual void CastVote(MafiaPlayer onPlayer, MafiaPlayer votingPlayer)
+	{
+		Votes[votingPlayer] = onPlayer;
+	}
+
+	public virtual Dictionary<MafiaPlayer, MafiaPlayer> GetVoteTable()
+	{
+		return Votes;
+	}
+}
