c76ba831f45092e1cf5483ff48424b4eb57f6dda
Cameron Smith <805camerons@gmail.com>
2022-04-20 20:24:36 -0400

    Create GlyphIcons for control displays

---
+++ code/Hud/Components/GlyphIcon.cs
@@ -0,0 +1,21 @@
+﻿using Sandbox;
+using Sandbox.UI;
+namespace TerryTrials.Hud.Components;
+
+public class GlyphIcon : Panel
+{
+	readonly Image GlyphImage;
+
+	public GlyphIcon(InputButton button, InputGlyphSize size)
+	{
+		GlyphImage = new Image()
+		{
+			Texture = Input.GetGlyph( button, size ),
+			Parent = this
+		};
+
+		GlyphImage.Style.Width = GlyphImage.Texture.Width;
+		GlyphImage.Style.Height = GlyphImage.Texture.Height;
+	}
+}
+
