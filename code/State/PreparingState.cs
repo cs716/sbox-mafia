84f0df4aa8164243fb4a3e5d0d4e05ec33dc77c9
Cameron Smith <805camerons@gmail.com>
2022-04-21 16:26:55 -0400

    Add PreparingState to handle role assignments

---
+++ code/State/PreparingState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerryTrials.State
+{
+	public partial class PreparingState : BaseState
+	{
+		public override void OnStart()
+		{
+			Log.Info( "PreparingState Started" );
+			base.OnStart();
+		}
+	}
+}
