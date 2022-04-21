1933631108aedbbc32e624cd74d03c121f438efa
Cameron Smith <805camerons@gmail.com>
2022-04-20 20:25:02 -0400

    Add player nametags to HUD

---
+++ code/Hud/World/Player/NameTagHandler.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox;
+using System.Collections.Generic;
+using System.Linq;
+using TerryTrials.Player;
+
+namespace TerryTrials.Hud.World.Player;
+
+public partial class NameTagHandler
+{
+	public Dictionary<Entity, NameTagPanel> NameTags { get; set; } = new();
+
+	public NameTagHandler()
+	{
+		Event.Register( this );
+	}
+
+	[Event.Tick]
+	public void OnTick()
+	{
+		if (Host.IsClient)
+		{
+			foreach ( MafiaPlayer player in Entity.All.OfType<MafiaPlayer>().Where( p => p.IsAlive ) )
+			{
+				if ( !NameTags.ContainsKey( player ) )
+				{
+					var nametag = new NameTagPanel( player );
+					NameTags.Add( player, nametag );
+				}
+			}
+		}
+	}
+}
+
---
+++ code/Hud/World/Player/NameTagPanel.cs
@@ -0,0 +1,45 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+using TerryTrials.Player;
+
+namespace TerryTrials.Hud;
+
+public partial class NameTagPanel : WorldPanel
+{
+	private readonly MafiaPlayer Player;
+	private readonly Label NameLabel;
+	public readonly Label StatusLabel;
+
+	public NameTagPanel(MafiaPlayer player)
+	{
+		this.Player = player;
+		NameLabel = Add.Label( "Loading ..", "NameTag" );
+		StatusLabel = Add.Label( string.Empty, "StatusLabel" );
+		StyleSheet.Load( "/Hud/World/Player/NameTagPanel.scss" );
+	}
+
+	public override void Tick()
+	{
+		if ( !Player.IsValid() || Player is null || !Player.IsAlive )
+		{
+			Delete( true );
+			return;
+		}
+
+		NameLabel.Text = Player.Client.Name;
+		var playerPosition = CurrentView.Position;
+		playerPosition.z = Position.z;
+
+		var targetRotation = Rotation.LookAt( playerPosition - Position );
+		var transform = new Transform( Player.Position + Vector3.Up * 100, Player.Rotation );
+		transform.Rotation = Rotation.Lerp( transform.Rotation, targetRotation, 1f );
+
+		Transform = transform;
+
+		StatusLabel.SetClass( "hide", StatusLabel.Text == string.Empty );
+		base.Tick();
+	}
+}
+
+
