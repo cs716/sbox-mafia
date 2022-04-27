771e88a8191366789bbbffe1d44cf04d9561c32a
Cameron Smith <805camerons@gmail.com>
2022-04-27 19:02:12 -0400

    Add Role Assignment menu

---
+++ code/Hud/Menu/RoleAssignedMenu.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox.UI;
+
+namespace TerryTrials.Hud.Menu
+{
+	[UseTemplate]
+	public partial class RoleAssignedMenu : Panel
+	{
+		public Label Role { get; set; }
+		public string RoleDescription { get; set; }
+		public bool Show = false;
+
+		public override void Tick()
+		{
+			SetClass( "hide", !Show );
+			base.Tick();
+		}
+	}
+}
