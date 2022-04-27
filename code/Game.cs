1556c62228401acfb5adac2ef4e21e8962fcefdc
Cameron Smith <805camerons@gmail.com>
2022-04-27 19:04:02 -0400

    Reformat Game variables

--- code/Game.cs
+++ code/Game.cs
@@ -5,6 +5,7 @@ using TerryTrials.Player;
 using TerryTrials.State;
 
 using System.Linq;
+using TerryTrials.Teams;
 
 namespace TerryTrials;
 /// <summary>
@@ -22,17 +23,39 @@ partial class Game : Sandbox.Game
 	}
 
 	public NameTagHandler NameTagHandler;
+	public HudController HudController;
+	public LocalKnowns LocalKnowns; // Client only - Controls "Known" players
+
+	public TownTeam TownTeam = new();
+	public MafiaTeam MafiaTeam = new(); 
+	public BaseTeam[] AddonTeams;
+	
+	[ServerVar( "tt_mafia_player_ratio", Help = "Ratio of Town to Mafia players", Min = 0.2f, Max = 0.4f )]
+	public static float MafiaPlayerRatio { get; set; } = 0.25f;
+	[ServerVar( "tt_doctor_player_ratio", Help = "Ratio of Town to Doctor players", Min = 0.2f, Max = 0.4f )]
+	public static float DoctorPlayerRatio { get; set; } = 0.2f;
+	[ServerVar( "tt_detective_player_ratio", Help = "Ratio of Town to Detective players", Min = 0.2f, Max = 0.4f )]
+	public static float DetectivePlayerRatio { get; set; } = 0.2f;
 
 	public Game()
 	{
 		if ( IsServer )
 		{
 			Global.TickRate = 30;
-			_ = new HudController();
+			HudController = new HudController();
+			//InitTeams();
+		} else if (IsClient)
+		{
+			LocalKnowns = new();
 		}
 		ChangeState( new LobbyState() );
 		NameTagHandler = new();
 	}
+
+	/*public void InitTeams()
+	{
+		// FUTURE: Any teams besides Mafia and Town will go here
+	}*/
 
 	/// <summary>
 	/// A client has joined the server. Make them a pawn to play with
@@ -47,31 +70,9 @@ partial class Game : Sandbox.Game
 		GameState?.OnPlayerJoin( player );
 	}
 
-	public override void PostLevelLoaded()
-	{
-		base.PostLevelLoaded();
-	}
-
-	[ServerCmd(name: "transmitLocal")]
-	public static void TestTransmitLocal()
-	{
-		All.OfType<MafiaPlayer>().Where( p => p.IsAlive ).ToList().ForEach( player =>
-		{
-			player.Transmit = TransmitType.Owner;
-		} );
-	}
-
-	[ServerCmd( name: "transmitGlobal" )]
-	public static void TestTransmitGlobal()
-	{
-		All.OfType<MafiaPlayer>().ToList().ForEach( player =>
-		{
-			player.Transmit = TransmitType.Always;
-		} );
-	}
-
 	public override void ClientDisconnect( Client cl, NetworkDisconnectionReason reason )
 	{
+		GameState?.OnPlayerLeave( cl );
 		base.ClientDisconnect( cl, reason );
 	}
 
@@ -79,5 +80,13 @@ partial class Game : Sandbox.Game
 	{
 		lastState?.Finish();
 		newState?.Start();
+	}
+
+	[ServerCmd(name: "overlay_test")]
+	public static void TestOverlay()
+	{
+		var player = ConsoleSystem.Caller.Pawn as MafiaPlayer;
+		var team = new MafiaTeam();
+		DisplayPlayerRoleToPlayer( To.Single( player ), team.UseFakeIdentity == true ? team.FakeSingularName : team.SingularName, team.Color, team.Description, true );
 	}
 }
